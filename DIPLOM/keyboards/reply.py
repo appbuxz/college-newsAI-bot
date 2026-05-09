c3258ded6e50828d4eddd38a200704be95f680b9
appbuxz <paulsyrtsev@mail.ru>
2026-05-09 22:14:44 +1000

    Delete DIPLOM directory

--- DIPLOM/keyboards/reply.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-main_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="Регистрация")]
-    ],
-    resize_keyboard=True
-)
